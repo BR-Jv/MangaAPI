b44713d5a0ad049d0fc10e6553db3b06ece16338
João.Victor <jv.alves193@gmail.com>
2025-03-07 01:09:57 -0300

    Criação da model Mangá

--- apps/mangaapi/models.py
+++ apps/mangaapi/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+DEMO_CHOICES = [
+    ("SN", "Shounen"),
+    ("SJ", "Shoujo"),
+    ("SE", "Seinen"),
+    ("JO", "Josei"),
+]
+
+class Manga(models.Model):
+    cadastrado = models.DateTimeField(auto_now_add=True)
+    nome = models.CharField(max_length=255) 
+    volume = models.IntegerField() #! verificar validations 
+    capitulo = models.IntegerField() #! verificar validations
+    demografia = models.CharField(choices=DEMO_CHOICES, max_length=7)
+
+
+    class meta: 
+        ordering = ['cadastrado']
